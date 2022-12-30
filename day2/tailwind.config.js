23019c277b1599df20b2be666082a570083242ff
JoaoSaIvador <j.salvador@kigroup.de>
2022-12-30 10:56:30 +0000

    feat: day 2 challenge design

--- day2/tailwind.config.js
+++ day2/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {},
+      boxShadow: {
+        1: '0px 7px 29px 0px rgba(100, 100, 111, 0.2)',
+        2: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;',
+      },
     },
   },
   plugins: [],
