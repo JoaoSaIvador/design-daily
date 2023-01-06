e01ff4ce0aba89dd8168e1b26cf31ac9797ff919
JoaoSaIvador <j.salvador@kigroup.de>
2023-01-06 14:39:27 +0000

    feat: day7 design

--- day7/pages/index.tsx
+++ day7/pages/index.tsx
@@ -3,24 +3,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faChevronLeft,
 	faEllipsisVertical,
+	faPause,
+	faShuffle,
+	faAnglesLeft,
+	faAnglesRight,
+	faInfinity,
 } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
 	return (
-		<div className="h-screen bg-[#898A92] flex justify-center items-center py-8">
-			<div className="flex flex-col justify-center items-center">
-				<div className="flex flex-col p-4 bg-[#151d1f]">
+		<div className="h-screen bg-gradient-to-br from-[#7296b7] to-[#0c1017] flex justify-center items-center py-8">
+			<div className="flex flex-col justify-center items-center shadow-1 rounded-3xl">
+				<div className="flex flex-col p-4 bg-[#181d26] rounded-t-3xl">
 					<div className="flex flex-row justify-between pb-10">
 						<button className="rounded-full p-2 border-[0.5px] border-[#39659a] w-[30px] h-[30px] flex justify-center items-center">
 							<FontAwesomeIcon
 								icon={faChevronLeft}
-								className="text-white text-sm"
+								className="text-[#e2e2e2] text-sm"
 							/>
 						</button>
 						<button className="rounded-full p-2 border-[0.5px] border-[#39659a] w-[30px] h-[30px] flex justify-center items-center">
 							<FontAwesomeIcon
 								icon={faEllipsisVertical}
-								className="text-white text-sm"
+								className="text-[#e2e2e2] text-sm"
 							/>
 						</button>
 					</div>
@@ -28,8 +33,8 @@ export default function Home() {
 						<Image
 							src="/cover.webp"
 							alt=""
-							width={200}
-							height={200}
+							width={250}
+							height={250}
 							className="shadow-[0px_0px_25px_25px_rgba(0,0,0,0.5)]"
 						/>
 						<h2 className="text-[#e2e2e2] font-medium text-xl pt-4">
@@ -45,8 +50,65 @@ export default function Home() {
 					min="0"
 					max="100"
 					value="70"
-					className="w-full m-[-10px]"
+					className="w-full m-[-10px] z-10"
+					readOnly
 				/>
+				<div className="bg-gradient-to-br from-[#1a2733] to-[#2e4153] w-full px-4 pb-4 rounded-b-3xl">
+					<div className="flex flex-row justify-center items-center mt-4">
+						<button className="rounded-full p-2 border-[0.5px] border-[#39659a] w-[40px] h-[40px] m-2 flex justify-center items-center shadow-[0px_5px_25px_1px_rgba(0,0,0,0.3)]">
+							<FontAwesomeIcon
+								icon={faShuffle}
+								className="text-[#e2e2e2] text-sm"
+							/>
+						</button>
+						<button className="rounded-full p-2 border-[0.5px] border-[#39659a] w-[40px] h-[40px] m-2 flex justify-center items-center shadow-[0px_5px_25px_1px_rgba(0,0,0,0.3)]">
+							<FontAwesomeIcon
+								icon={faAnglesLeft}
+								className="text-[#e2e2e2] text-sm"
+							/>
+						</button>
+						<button className="rounded-full p-2 border-[0.5px] border-[#39659a] w-[40px] h-[40px] m-2 flex justify-center items-center shadow-[0px_5px_25px_1px_rgba(0,0,0,0.3)]">
+							<FontAwesomeIcon
+								icon={faPause}
+								className="text-[#e2e2e2] text-sm"
+							/>
+						</button>
+						<button className="rounded-full p-2 border-[0.5px] border-[#39659a] w-[40px] h-[40px] m-2 flex justify-center items-center shadow-[0px_5px_25px_1px_rgba(0,0,0,0.3)]">
+							<FontAwesomeIcon
+								icon={faAnglesRight}
+								className="text-[#e2e2e2] text-sm"
+							/>
+						</button>
+						<button className="rounded-full p-2 border-[0.5px] border-[#39659a] w-[40px] h-[40px] m-2 flex justify-center items-center shadow-[0px_5px_25px_1px_rgba(0,0,0,0.3)]">
+							<FontAwesomeIcon
+								icon={faInfinity}
+								className="text-[#e2e2e2] text-sm"
+							/>
+						</button>
+					</div>
+					<div className="max-h-[215px] overflow-y-scroll p-2">
+						<div className="flex flex-row justify-between items-center p-2 m-2 text-[#cccccc] font-thin text-lg border-b-[2.5px] border-[#181d26] border-opacity-40">
+							<span>Supercell</span>
+							<span>05:13</span>
+						</div>
+						<div className="flex flex-row justify-between items-center p-2 m-2 text-[#cccccc] font-thin text-lg border-b-[2.5px] border-[#181d26] border-opacity-40">
+							<span>Tech 49</span>
+							<span>03:11</span>
+						</div>
+						<div className="flex flex-row justify-between items-center p-2 m-2 text-[#cccccc] font-thin text-lg border-b-[2.5px] border-[#181d26] border-opacity-40">
+							<span>The Library</span>
+							<span>04:24</span>
+						</div>
+						<div className="flex flex-row justify-between items-center p-2 m-2 text-[#cccccc] font-thin text-lg border-b-[2.5px] border-[#181d26] border-opacity-40">
+							<span>Horatius</span>
+							<span>03:55</span>
+						</div>
+						<div className="flex flex-row justify-between items-center p-2 m-2 text-[#cccccc] font-thin text-lg border-b-[2.5px] border-[#181d26] border-opacity-40">
+							<span>Oblivion Soundtrack</span>
+							<span>04:32</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- day7/tailwind.config.js
+++ day7/tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-			boxShadow: {},
+			boxShadow: {
+				1: 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;',
+			},
 		},
 	},
 	plugins: [],
